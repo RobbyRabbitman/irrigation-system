9fbb725b221ca88e1b1017ebbae7ba03e770856e
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-12-29 14:22:47 +0100

    auth guard: impl

--- apps/api/src/app/guards/authenticated-user.guard.ts
+++ apps/api/src/app/guards/authenticated-user.guard.ts
@@ -1,11 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { PassportRequest } from '../model/Passport';
 
 @Injectable()
 export class AuthenticatedUserGuard implements CanActivate {
   canActivate(
     context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    return context.switchToHttp().getRequest<PassportRequest>()?.user
+      ?.authenticated;
   }
 }
