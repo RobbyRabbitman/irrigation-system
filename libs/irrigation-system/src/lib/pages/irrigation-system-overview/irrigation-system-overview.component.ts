674be48a4b4349a566235e5cabe60cae1f47fd2b
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-01-01 23:25:57 +0100

    web: irrigation systems: auto nav to detail

--- libs/irrigation-system/src/lib/pages/irrigation-system-overview/irrigation-system-overview.component.ts
+++ libs/irrigation-system/src/lib/pages/irrigation-system-overview/irrigation-system-overview.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IrrigationSystem } from '@irrigation/generated/client';
 import { StoreService } from '@irrigation/shared/store';
+import { isNonNull } from '@irrigation/shared/util';
+import { filter, take } from 'rxjs';
 import { ROUTE_IRRIGATION_SYSTEM } from '../../routes/routes';
 import { IrrigationSystemComponent } from '../irrigation-system/irrigation-system.component';
 
@@ -10,12 +12,22 @@ import { IrrigationSystemComponent } from '../irrigation-system/irrigation-syste
   templateUrl: './irrigation-system-overview.component.html',
   styleUrls: ['./irrigation-system-overview.component.scss'],
 })
-export class IrrigationSystemOverviewComponent {
-  constructor(
+export class IrrigationSystemOverviewComponent implements OnInit {
+  public constructor(
     public readonly store: StoreService,
     private readonly router: Router,
     private readonly route: ActivatedRoute
   ) {}
+
+  public ngOnInit(): void {
+    this.store.user$.pipe(filter(isNonNull), take(1)).subscribe({
+      next: ({ irrigationSystems }) =>
+        irrigationSystems?.length === 1
+          ? this.details(irrigationSystems[0])
+          : undefined,
+    });
+  }
+
   public details({ id }: IrrigationSystem) {
     this.router.navigate([`../${ROUTE_IRRIGATION_SYSTEM}`], {
       relativeTo: this.route,
